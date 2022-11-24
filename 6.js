118879d69e590d6b04ac780b35814d6de5c78e6a
Roman <47541355+RLobodenko@users.noreply.github.com>
2022-11-24 07:09:07 +0300

    Update 6.js

--- 6.js
+++ 6.js
@@ -9,16 +9,16 @@ function f3(){
     var a = document.getElementById('in4').value;
     var b = document.getElementById('in5').value;
 
-    var c = (a - b)/b * 100
+    var c = (a - b)/b * 100;
    
     document.getElementById('out1').innerHTML = c + " %";
 
-    var d = a*60/hz
+    var d = a*60/hz;
     
      document.getElementById('out').innerHTML = d + " м3/ч";
     
     
-    var e = a*68/hz
+    var e = a*68/hz;
      document.getElementById('out2').innerHTML = e + " м3/ч";
 }
 
