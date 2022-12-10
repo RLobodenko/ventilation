6c02c24507ce502d63f686dc25c07f45eed59b06
Roman <47541355+RLobodenko@users.noreply.github.com>
2022-12-10 04:06:24 +0300

    Update 6.js

--- 6.js
+++ 6.js
@@ -1,7 +1,7 @@
 
 var but1 = document.getElementById('but1');
 
-
+var but2 = document.getElementById('but2');
 
 
 function f3(){
@@ -17,6 +17,18 @@ function f3(){
     
 }
 
+function f4(){
+    
+    var a = document.getElementById('in6').value;
+    var b = document.getElementById('in7').value;
+
+    var c = 2*a*b/(a+b);
+   
+    document.getElementById('out2').innerHTML = c + " %";
+    
+   
+    
+}
 
 
 
