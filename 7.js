3c56ebf7a43b83e473fe4807e1f36c1552d19d8f
Roman <47541355+RLobodenko@users.noreply.github.com>
2023-05-06 08:02:51 +0300

    Update 7.js

--- 7.js
+++ 7.js
@@ -12,26 +12,21 @@ radio1[5].onchange = f2;
 radio1[6].onchange = f2;
 
 function f2() {
-for (i=0; i<8;i++){
+for (i=0; i<7;i++){
 if (radio1[i].checked) {
-	document.getElementById('one').src="planes.jpg";
-	document.getElementById('out').innerHTML = "25000 руб";
-	document.getElementById('two').src="Group47.png";
+	document.getElementById('one').src="IMG_3041.JPG";
+
 	return;
 }
 
 else if (radio1[1].checked){ 
-	document.getElementById('one').src="planes_1.jpg";
-	document.getElementById('out').innerHTML = "40000 руб";
-	document.getElementById('two').src="Group47_1.png";
+		document.getElementById('one').src="IMG_3035.JPG";
 		return;	
 }
 
 
 else if (radio1[2].checked){ 
-	document.getElementById('one').src="planes_2.jpg";
-	document.getElementById('out').innerHTML = "38000 руб";
-	document.getElementById('two').src="Group47.png";
+		document.getElementById('one').src="IMG_3031.JPG";
 		return;	
 }
 
@@ -42,31 +37,18 @@ else if (radio1[3].checked){
 		return;	
 }
 else if (radio1[4].checked){ 
-	document.getElementById('one').src="planes_1.jpg";
-	document.getElementById('out').innerHTML = "40000 руб";
-	document.getElementById('two').src="Group47_1.png";
+	document.getElementById('one').src="IMG_3018.JPG";
 		return;	
 }
 else if (radio1[5].checked){ 
-	document.getElementById('one').src="planes_2.jpg";
-	document.getElementById('out').innerHTML = "38000 руб";
-	document.getElementById('two').src="Group47.png";
+	document.getElementById('one').src="IMG_3009.JPG";
 		return;	
 }
 else if (radio1[6].checked){ 
-	document.getElementById('one').src="planes_1.jpg";
-	document.getElementById('out').innerHTML = "40000 руб";
-	document.getElementById('two').src="Group47_1.png";
+		document.getElementById('one').src="IMG_2944.JPG";
 		return;	
 }
-
-
-else {
-	document.getElementById('one').src="planes.jpg";
-	document.getElementById('out').innerHTML = "25000 руб";
-	document.getElementById('two').src="Group47.png";
-		return;	
-}	
+	
 }	
 }
 
