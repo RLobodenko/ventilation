d511e4be7b17cbacf5f68c1413f9f10b8705915a
Roman <47541355+RLobodenko@users.noreply.github.com>
2023-05-06 08:19:14 +0300

    Update 7.js

--- 7.js
+++ 7.js
@@ -3,6 +3,7 @@
 
 var radio1 = document.getElementsByName('in1');
 
+radio1[0].onchange = f2;
 radio1[1].onchange = f2;
 radio1[2].onchange = f2;
 radio1[3].onchange = f2;
@@ -11,7 +12,7 @@ radio1[5].onchange = f2;
 
 function f2() {
 for (i=0; i<6;i++){
-if (radio1[i].checked) {
+if (radio1[0].checked) {
 	document.getElementById('one').src="IMG_3041.JPG";
 
 	return;
